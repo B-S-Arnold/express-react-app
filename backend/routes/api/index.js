433827031b0857b68173ab6294a1754a005120cf
Bryan <bryanscottarnold@gmail.com>
2022-02-03 16:06:51 -0500

    fixed files, user auth

---
+++ backend/routes/api/index.js
@@ -0,0 +1,35 @@
+
+const router = require('express').Router();
+
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models')
+const asyncHandler = require('express-async-handler');
+const { requireAuth } = require('../../utils/auth.js');
+
+router.post('/test', function (req, res) {
+    res.json({ requestBody: req.body });
+});
+
+// GET /api/set-token-cookie
+
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
+
+// GET /api/require-auth
+
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
+module.exports = router;
