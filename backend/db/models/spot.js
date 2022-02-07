378578013e187a2ae3c7396ba9c6967787c1eddd
Bryan <bryanscottarnold@gmail.com>
2022-02-07 16:12:39 -0500

    updated model files

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -1,15 +1,40 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Spot = sequelize.define('Spot', {
-    userId: DataTypes.INTEGER,
-    address: DataTypes.STRING,
-    city: DataTypes.STRING,
-    state: DataTypes.STRING,
-    country: DataTypes.STRING,
-    lat: DataTypes.DECIMAL,
-    lng: DataTypes.DECIMAL,
-    name: DataTypes.STRING,
-    price: DataTypes.DECIMAL
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    address: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    city:  {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    state: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    country: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    lat: {
+      type: DataTypes.DECIMAL,
+    },
+    lng: {
+      type: DataTypes.DECIMAL,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    price: {
+      allowNull: false,
+      type: DataTypes.DECIMAL
+    },
   }, {});
   Spot.associate = function(models) {
     // associations can be defined here
