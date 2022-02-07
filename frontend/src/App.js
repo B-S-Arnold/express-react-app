1714ecf0a66efa93257d0480dfd013551f00ce85
Bryan <bryanscottarnold@gmail.com>
2022-02-07 06:35:59 -0500

    nav and button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -12,18 +13,23 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  return isLoaded && (
-    <Switch>
-      <Route path={["/login", "/log-in"]}>
-        <LoginFormPage />
-      </Route>
-      <Route path={["/signup", "/sign-up"]}>
-        <SignupFormPage />
-      </Route>
-      <Route>
-        Page Not Found
-      </Route>
-    </Switch>
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path={["/login", "/log-in"]}>
+            <LoginFormPage />
+          </Route>
+          <Route path={["/signup", "/sign-up"]}>
+            <SignupFormPage />
+          </Route>
+          <Route>
+            Page Not Found
+          </Route>
+        </Switch>
+      )}
+    </>
   );
 }
 
