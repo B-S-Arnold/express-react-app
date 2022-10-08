c4600b93a645cf8b166364e9184c17e268fe2e4f
Bryan <bryanscottarnold@gmail.com>
2022-10-07 22:38:54 -0400

    blurb

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -77,11 +77,9 @@ function SplashPage() {
                     <select
                         id='ads'
                         type='select'
-                        // placeholder='2'
-                        s
                         className='sin'>
                             <option value='1'>1</option>
-                            <option value='2'>2</option>
+                            <option value='2' selected>2</option>
                             <option value='3'>3</option>
                             <option value='4'>4</option>
                             <option value='5'>5</option>
