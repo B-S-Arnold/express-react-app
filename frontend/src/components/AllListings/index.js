107467bd3805db10f865216555877a2428b66c22
Bryan <bryanscottarnold@gmail.com>
2022-10-28 23:40:07 -0400

    scroll jump

--- frontend/src/components/AllListings/index.js
+++ frontend/src/components/AllListings/index.js
@@ -62,23 +62,9 @@ function AllListingsPage() {
         if (spot !== null) {
             const thisSpot = spot
 
-            // function to display image at array position
+            // const spotImgDisplay = 
 
-            const thisSpotArr = imgArr?.filter(img => img?.spotId === thisSpot?.id)
-            let i = 0
-            let thisImage = thisSpotArr[i]
-            let conditionalBtns = (<></>)
-            if (thisSpotArr.length > 1){
-
-                if (i !== thisSpotArr.length -1){
-                    let leftbtn = 
-                }
-
-                if (i !== 0) {
-
-                }
-
-            }
+            
 
             let allImages = () => imgArr.map((image) => {
 
